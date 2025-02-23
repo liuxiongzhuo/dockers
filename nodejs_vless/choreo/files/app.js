c144b3a1edc593b6e480711fe64aa671ea33aa7c
刘雄卓 <3231729413@qq.com>
2025-02-23 16:23:33 +0800

    Update app.js

--- nodejs_vless/choreo/files/app.js
+++ nodejs_vless/choreo/files/app.js
@@ -6,22 +6,22 @@ const proxy = httpProxy.createProxyServer({});
 
 // 创建 HTTP 服务器，处理请求并将它们代理到 3000 端口
 const server = http.createServer((req, res) => {
-    if (req.url === '/') {
+    if (req.url !== '/') {
+        // 不是根路径 '/'，代理请求到 localhost:3000
+        proxy.web(req, res, { target: 'http://localhost:3000' }, (error) => {
+            if (error) {
+                console.error('Proxy error:', error);
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Internal Server Error');
+            }
+        });
+    } else {
         // 如果请求是根路径 '/', 返回 'hello'
         res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.end('hello');
     }
 });
-server.on('upgrade', (req, socket, head) => {
-    // 代理 WebSocket 请求到 localhost:3000
-    proxy.ws(req, socket, head, { target: 'http://localhost:3000' }, (error) => {
-        if (error) {
-            console.error('WebSocket Proxy error:', error);
-            socket.write('HTTP/1.1 500 Internal Server Error\r\n\r\n');
-            socket.end();
-        }
-    });
-});
+
 console.log("start");
 exec("nohup ./xray run -c server.json &", function (err, stdout, stderr) {
     if (err) {
