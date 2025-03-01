681fcd9b38a2cda7a998b4846b337d523ae48052
meteorrain <liuxiongzhuo@163.com>
2025-03-01 11:58:14 +0800

    adf

--- warp_vless/chereo/files/app.sh
+++ warp_vless/chereo/files/app.sh
@@ -1,5 +1,5 @@
 #! sh
 nohup ./sing-box-1.11.4-linux-amd64/sing-box run -c ./config.json &
-nohup ./c tunnel --url tcp://127.0.0.1:5353 & 
+nohup ./c tunnel --url tcp://127.0.0.1:5353 --protocol auto & 
 sleep 5
 ./web
